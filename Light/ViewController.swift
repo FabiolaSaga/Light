26c588016f159ccc282fd85fa0d8a7908dbeedc5
FabiolaSaga <saga.fabiola@gmail.com>
2019-01-26 11:25:32 -0600

    Expand button size to cover screen, remove text from button

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+//        if lightOn {
+//            view.backgroundColor = .white
+//
+//        } else {
+//            view.backgroundColor = .black
+//
+//        }
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
 }
 
